6ff2f53051ec611d0553bf0b50be6e96e048848a
jeongnangju <angjujjj@gmail.com>
2025-07-28 07:29:42 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,15 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      }
-    }
+    outDir: 'dist'
   },
+  base: './',
   server: {
-    historyApiFallback: true
+    port: 3000
   }
 })
+
