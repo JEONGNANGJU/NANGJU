b6745f716664c09fdbeb2bac037b6b6a20cb8358
jeongnangju <angjujjj@gmail.com>
2025-07-28 07:41:48 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
+    outDir: 'dist', // 중요! Vercel이 이 폴더를 배포 폴더로 씀
   },
+  base: './' // 상대 경로로 해줘야 index.html 등 참조됨
 })
