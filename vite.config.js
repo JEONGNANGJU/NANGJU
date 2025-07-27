2c737711108ce59866ab7cb6ebb541d42b01ebc4
jeongnangju <angjujjj@gmail.com>
2025-07-28 08:22:17 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: "./", // ← 이 부분이 핵심!
   plugins: [react()],
-  build: {
-    outDir: 'dist'
-  },
-  base: './'
 })
