2824b84122a507825611de80973f4965a31da0ba
jeongnangju <angjujjj@gmail.com>
2025-07-28 08:57:50 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './', // 로컬과 Vercel에서 모두 동작하게
+  base: './', 
   plugins: [react()],
 })
