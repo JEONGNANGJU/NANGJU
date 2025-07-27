63879632bfa61d9e7067fe9277fa427e5a4a73ce
jeongnangju <angjujjj@gmail.com>
2025-07-28 08:49:38 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './', // 또는 "/NANGJU/" → Vercel에서는 "./"이 일반적
+  base: '/NANGJU/',
   plugins: [react()],
 })
